dda722a396c2a63092e3a5fd8a61af9f2b698c81
Prasanna Gautam <prasannagautam@gmail.com>
2012-11-05 14:03:49 -0500

    xmonad doesn't lose cursor when using swing applications and others too

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -19,12 +19,51 @@ import XMonad.Layout.ResizableTile
 import XMonad.Layout.IM
 import XMonad.Layout.PerWorkspace (onWorkspace)
 import XMonad.Hooks.SetWMName
+import XMonad.Layout.ShowWName
 import XMonad.Hooks.ManageDocks
 import XMonad.Hooks.ManageHelpers
 import XMonad.Hooks.UrgencyHook
 import XMonad.Util.EZConfig
 import XMonad.Util.Run(spawnPipe)
 import Data.Ratio ((%))
+import qualified XMonad.StackSet as W
+import Control.Monad
+
+{- 
+
+  This part is taken from XMonad.Hooks.ICCMFocus. 
+  xmonad-contrib seems to be broken for some Ubuntu versions
+
+-}
+atom_WM_TAKE_FOCUS ::
+  X Atom
+
+atom_WM_TAKE_FOCUS =
+  getAtom "WM_TAKE_FOCUS"
+
+takeFocusX :: 
+  Window
+  -> X()
+
+takeFocusX w =
+  withWindowSet . const $ do
+    dpy       <- asks display
+    wmtakef   <- atom_WM_TAKE_FOCUS
+    wmprot    <- atom_WM_PROTOCOLS
+    protocols <- io $ getWMProtocols dpy w
+    when (wmtakef `elem` protocols) $
+      io . allocaXEvent $ \ev -> do
+          setEventType ev clientMessage
+          setClientMessageEvent ev w wmprot 32 wmtakef currentTime
+          sendEvent dpy w False noEventMask ev
+
+takeTopFocus :: 
+  X()
+
+takeTopFocus = 
+  (withWindowSet $ maybe (setFocusX =<< asks theRoot) takeFocusX . W.peek) >> setWMName "LG3D"  
+
+{- end stuff from ICCCMFocus -}
 
 
 {- 
@@ -37,15 +76,17 @@ myFocusedBorderColor = "#ff0000" -- focused Window color
 myNormalBorderColor = "#cccccc" -- inactive border color
 myBorderWidth =1 
 myIMRosterTitle = "Contacts"
+myLayout = avoidStruts $ layoutHook defaultConfig
+myLogHook = takeTopFocus
 
 
 myWorkspaces = 
-  [ "0:Dev", "1:Chat", "2:Web",
-    "3:VM", "4:Debug", "5",
+  [ "0:Debug", "1:Dev", "2:Web",
+    "3:VM", "4:Chat", "5",
     "6", "7", "8", "9"
   ]
 
-startupWorkspace = "0:Dev"
+startupWorkspace = "1:Dev"
 
 
 
@@ -55,9 +96,9 @@ main = do
   xmproc <- spawnPipe "xmobar ~/.xmonad/xmobarrc"
   xmonad $ withUrgencyHook NoUrgencyHook $ defaultConfig {
     startupHook = setWMName "LG3D" -- required for Java Swing applications to run properly
-    , layoutHook = avoidStruts $ layoutHook defaultConfig
+    , layoutHook = showWName $ myLayout
     , focusedBorderColor = myFocusedBorderColor
     , borderWidth = myBorderWidth
     , modMask = myModMask
-  } `additionalKeys`
-  [ ((myModMask .|. shiftMask, xK_z), spawn "gnome-screensaver-command --lock")]
+    , logHook = myLogHook
+  } 
