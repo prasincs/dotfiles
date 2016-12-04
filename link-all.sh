71ef02b1099f9a298db9237c064df0ce2db9b295
Prasanna Gautam <prasannagautam@gmail.com>
2016-12-04 12:32:43 -0800

    syncing all files

--- link-all.sh
+++ link-all.sh
@@ -27,14 +27,16 @@ function link_dir {
   fi
 }
 
+if []
+
 
 ln -s $DIR/tmux/tmux.conf $HOME/.tmux.conf
 ln -s $DIR/tmux/tmux.shared $HOME/.tmux.shared
 link_file zsh/zshrc $HOME/.zshrc
-link_dir xmonad $HOME/.xmonad
-link_dir irssi $HOME/.irssi
-link_file xsession $HOME/.xsession
-link_file emacs $HOME/.emacs
+#link_dir xmonad $HOME/.xmonad
+#link_dir irssi $HOME/.irssi
+#link_file xsession $HOME/.xsession
+#link_file emacs $HOME/.emacs
 link_file gitconfig $HOME/.gitconfig
 link_file gitignore_global $HOME/.gitignore_global
 for f in `ls $DIR/bin/*`; do
